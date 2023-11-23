80b2bd9a1b4c46bc2722bdb89c17a53887c9b994
Shurrd <engraaadeyemi@gmail.com>
2023-11-23 16:58:57 +0100

    nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,40 +3,87 @@ import Logo from '../assets/images/Logo.svg';
 import { Link } from 'react-router-dom';
 import { GoHome, GoGear } from 'react-icons/go';
 import { BiWorld } from 'react-icons/bi';
+import { LuMenu } from 'react-icons/lu';
+import { CgClose } from 'react-icons/cg';
+import navLinks from '../utils/navLinks';
 
 const Navbar = () => {
   const [menu, setMenu] = useState(true);
 
   const handleMenu = () => {
     setMenu((prev) => !prev);
+    console.log(menu);
   };
 
   return (
-    <nav className='px-8 lg:px-16 xl:px-32 py-5 flex justify-between items-center border border-b-[#EAECF0]'>
-      {menu ? (
-        <>
-          <div className='flex items-center gap-6'>
-            <Link to={'/'}>
-              <img src={Logo} alt='Air Quality' />
-            </Link>
-            <Link className='bg-[#53389E] text-white hidden  md:flex items-center rounded-md px-2 py-1 gap-2'>
-              <GoHome size={25} />
-              Overview
-            </Link>
-            <Link className='hidden md:flex items-center gap-2 text-[#a5a6a7]'>
-              <BiWorld size={25} />
-              Global Index
-            </Link>
-          </div>
-          <Link className='hidden items-center gap-2 md:flex'>
-            Settings
-            <GoGear size={25} />
+    <>
+      <nav className='px-8 lg:px-16 xl:px-32 py-5 flex justify-between items-center border border-b-[#EAECF0] bg-white'>
+        <div className='flex items-center gap-10'>
+          <Link to={'/'}>
+            <img src={Logo} alt='Air Quality' />
           </Link>
-        </>
-      ) : (
-        ''
-      )}
-    </nav>
+          <Link
+            className={
+              menu
+                ? 'bg-[#53389E] text-white hidden  md:flex items-center rounded-md px-2 py-1 gap-2 text-sm font-medium'
+                : 'hidden md:flex bg-[#53389E] text-white items-center rounded-md px-2 py-1 gap-2 text-sm font-medium'
+            }
+          >
+            <GoHome size={25} />
+            Overview
+          </Link>
+          <Link
+            className={
+              menu
+                ? 'hidden md:flex items-center gap-2 text-[#a5a6a7] text-sm font-semibold'
+                : 'hidden md:flex items-center gap-2 text-[#a5a6a7] text-sm font-semibold'
+            }
+          >
+            <BiWorld size={25} />
+            Global Index
+          </Link>
+        </div>
+        <Link
+          className={
+            menu
+              ? 'hidden items-center gap-2 md:flex text-sm font-semibold'
+              : 'hidden items-center gap-2 md:flex text-sm font-semibold'
+          }
+        >
+          <GoGear size={25} />
+          Settings
+        </Link>
+        <div
+          className={
+            menu ? 'md:hidden block cursor-pointer' : 'md:hidden cursor-pointer'
+          }
+          onClick={handleMenu}
+        >
+          {menu ? <CgClose size={25} /> : <LuMenu size={25} />}
+        </div>
+      </nav>
+      <div
+        className={
+          menu
+            ? 'md:hidden bg-white pb-10 px-7 py-4 flex flex-col gap-5 shadow-2xl'
+            : 'hidden'
+        }
+      >
+        {navLinks.map((item) => {
+          const { id, name, url, icon } = item;
+          const Icon = icon;
+          return (
+            <Link
+              key={id}
+              className='text-[#a5a6a7] flex items-center gap-2 text-sm font-semibold'
+            >
+              <Icon size={18} />
+              {name}
+            </Link>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
